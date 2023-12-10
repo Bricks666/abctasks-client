0e027f96568912b238ab1030e0b454b31489f593
Bricks666 <tconckoff82@gmail.com>
2023-12-10 20:47:33 +0300

    fix building

--- src/features/page/color-scheme-toggler/config.ts
+++ src/features/page/color-scheme-toggler/config.ts
@@ -3,17 +3,24 @@ import LightModeIcon from '@mui/icons-material/LightMode';
 import ModeStandbyIcon from '@mui/icons-material/ModeStandby';
 import * as React from 'react';
 
-import { ColorScheme } from './types';
+import { colorSchemeModel } from '@/shared/models';
 
-export const ICONS_MAP: Record<ColorScheme, React.ComponentType> = {
+export const ICONS_MAP: Record<
+	colorSchemeModel.ColorScheme,
+	React.ComponentType
+> = {
 	dark: DarkModeIcon,
 	system: ModeStandbyIcon,
 	light: LightModeIcon,
 };
 
 export interface Scheme {
-	readonly value: ColorScheme;
+	readonly value: colorSchemeModel.ColorScheme;
 	readonly icon: React.ComponentType;
 }
 
-export const SCHEMES: ColorScheme[] = ['dark', 'system', 'light'];
+export const SCHEMES: colorSchemeModel.ColorScheme[] = [
+	'dark',
+	'system',
+	'light'
+];
--- src/features/page/color-scheme-toggler/desktop-color-scheme-toggler.tsx
+++ src/features/page/color-scheme-toggler/desktop-color-scheme-toggler.tsx
@@ -2,11 +2,11 @@ import { ToggleButton, ToggleButtonGroup, Tooltip } from '@mui/material';
 import * as React from 'react';
 import { useTranslation } from 'react-i18next';
 
+import { colorSchemeModel } from '@/shared/models';
 import { CommonProps } from '@/shared/types';
 
 import { ICONS_MAP, SCHEMES } from './config';
 import { useToggleColorScheme } from './lib';
-import { ColorScheme } from './types';
 
 export interface DesktopColorSchemeTogglerProps extends CommonProps {
 	readonly size?: 'small' | 'medium' | 'large';
@@ -19,7 +19,7 @@ export const DesktopColorschemeToggler: React.FC<
 	const { t, } = useTranslation('common');
 	const [colorScheme, changeColorScheme] = useToggleColorScheme();
 
-	const onChange = (_event: unknown, value: ColorScheme) => {
+	const onChange = (_event: unknown, value: colorSchemeModel.ColorScheme) => {
 		changeColorScheme(value);
 	};
 
