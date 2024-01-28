761cd115e049e0f8430ce35bd114a72fcfdbaa55
Bricks666 <tconckoff82@gmail.com>
2024-01-28 23:09:57 +0300

    some fixes in configs

--- vite.config.ts
+++ vite.config.ts
@@ -3,68 +3,74 @@ import * as path from 'path';
 
 import { babel } from '@rollup/plugin-babel';
 import react from '@vitejs/plugin-react';
-import { defineConfig, splitVendorChunkPlugin } from 'vite';
+import { defineConfig, loadEnv, splitVendorChunkPlugin } from 'vite';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 const LOCALE_FILE_REGEXP = /src\/([\w-/ ])*\/([\w-]+)\/([\w-]+)\.json$/;
 
-export default defineConfig({
-	server: {
-		port: 3000,
-		cors: true,
-		proxy: {
-			'/api': {
-				changeOrigin: true,
-				target: 'http://localhost:5000',
-			},
-		},
-	},
-	preview: {
-		port: 3000,
-		cors: true,
-		proxy: {
-			'/api': {
-				changeOrigin: true,
-				target: 'http://localhost:5001',
-			},
-		},
-	},
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, 'src'),
-		},
-	},
-	css: {
-		devSourcemap: true,
-	},
-	plugins: [
-		react(),
-		babel({
-			babelrc: true,
-			configFile: true,
-			babelHelpers: 'bundled',
-			browserslistConfigFile: true,
-			extensions: ['.ts', '.tsx'],
-		}),
-		splitVendorChunkPlugin(),
-		viteStaticCopy({
-			targets: [
-				{
-					src: 'src/**/locales/**/*.json',
-					rename: (_name, _extension, path) => {
-						const matches = path.match(LOCALE_FILE_REGEXP);
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, '.', '');
 
-						if (matches.length < 4) {
-							return '';
-						}
-
-						const [, , language, namespace] = matches;
-
-						return `${language}/${namespace}.json`;
-					},
-					dest: 'locales',
+	return {
+		server: {
+			port: 3000,
+			cors: true,
+			proxy: {
+				'/api': {
+					changeOrigin: true,
+					target: env.API_HOST,
+					rewrite: (url) => url.replace('/api', ''),
 				},
-			],
-		}),
-	],
+			},
+		},
+		preview: {
+			port: 3000,
+			cors: true,
+			proxy: {
+				'/api': {
+					changeOrigin: true,
+					target: env.API_HOST,
+					rewrite: (url) => url.replace('/api', ''),
+				},
+			},
+		},
+		resolve: {
+			alias: {
+				'@': path.resolve(__dirname, 'src'),
+			},
+		},
+		css: {
+			devSourcemap: true,
+		},
+		plugins: [
+			react(),
+			babel({
+				babelrc: true,
+				configFile: true,
+				babelHelpers: 'bundled',
+				browserslistConfigFile: true,
+				extensions: ['.ts', '.tsx'],
+			}),
+			splitVendorChunkPlugin(),
+			viteStaticCopy({
+				targets: [
+					{
+						src: 'src/**/locales/**/*.json',
+						rename: (_name, _extension, path) => {
+							const matches = path.match(LOCALE_FILE_REGEXP);
+
+							if (matches.length < 4) {
+								return '';
+							}
+
+							const [, , language, namespace] = matches;
+
+							return `${language}/${namespace}.json`;
+						},
+						dest: 'locales',
+					},
+				],
+			}),
+		],
+	};
 });
