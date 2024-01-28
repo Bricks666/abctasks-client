a97ec5ba107b191087a8d0bea750d3e996a8cff9
Bricks666 <tconckoff82@gmail.com>
2024-01-28 23:09:57 +0300

    some fixes in configs

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,16 +1,8 @@
 import { defineConfig, devices } from '@playwright/test';
 import dotenv from 'dotenv';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
+dotenv.config({ path: './.env.test' });
 
-dotenv.config({ path: './.env.test.local' });
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
 	testDir: './e2e',
 	/* Run tests in files in parallel */
@@ -26,7 +18,7 @@ export default defineConfig({
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
-		baseURL: 'http://localhost:3000',
+		baseURL: process.env.BASE_CLIENT_URL,
 
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: 'on-first-retry',
@@ -63,11 +55,4 @@ export default defineConfig({
 		// 	use: { ...devices['iPhone 12'] },
 		// },
 	],
-
-	/* Run your local dev server before starting the tests */
-	webServer: {
-		command: 'npm run build && npm run preview',
-		url: 'http://localhost:3000',
-		reuseExistingServer: !process.env.CI,
-	},
 });
