9a9bfba27671b4c896b2e52de1c6de67f3c5d010
Bricks666 <tconckoff82@gmail.com>
2024-07-02 21:27:39 +0300

    update api host from env file

--- src/shared/configs/const/api.ts
+++ src/shared/configs/const/api.ts
@@ -1,4 +1,5 @@
 // eslint-disable-next-line import/no-mutable-exports
-export const base = import.meta.env.VITE_API_HOST ?? '';
+console.log(import.meta.env.VITE_API_HOST);
+export const BASE = import.meta.env.VITE_API_HOST;
 
-export const api = `${base}/api`;
+export const API_HOST = `${BASE}/api`;
