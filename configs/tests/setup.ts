c73aeb068fabbe3117dae10bcce01bd702c81f7d
Bricks666 <tconckoff82@gmail.com>
2024-07-02 21:18:00 +0300

    add mock i18n into tests preparation

--- configs/tests/setup.ts
+++ configs/tests/setup.ts
@@ -1,8 +1,13 @@
+import i18n from 'i18next';
 import { expect, afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
-import matchers from '@testing-library/jest-dom/matchers';
-console.log(matchers);
+import * as matchers from '@testing-library/jest-dom/matchers';
+
 expect.extend(matchers);
+
+i18n.init({
+	lng: 'cimode',
+});
 
 afterEach(() => {
 	cleanup();
