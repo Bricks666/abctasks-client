103bd7d8afff95321dc0e0fc38e6009c12f456ad
Bricks666 <tconckoff82@gmail.com>
2024-07-02 22:27:35 +0300

    remove extra options from vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,12 +11,6 @@ export default defineConfig({
 		coverage: {
 			reporter: ['text', 'json-summary', 'json'],
 			reportOnFailure: true,
-			thresholds: {
-				lines: 60,
-				branches: 60,
-				functions: 60,
-				statements: 60,
-			},
 		},
 	},
 	resolve: {
