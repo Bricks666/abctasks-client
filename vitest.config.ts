0fe349c158a5020965f98d35f74518ae399317a3
Bricks666 <tconckoff82@gmail.com>
2024-07-02 21:52:19 +0300

    add github action to vitest coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,6 +8,16 @@ export default defineConfig({
 		include: ['./src/**/*.spec.{ts,tsx}'],
 		clearMocks: true,
 		globals: true,
+		coverage: {
+			reporter: ['text', 'json-summary', 'json'],
+			reportOnFailure: true,
+			thresholds: {
+				lines: 60,
+				branches: 60,
+				functions: 60,
+				statements: 60,
+			},
+		},
 	},
 	resolve: {
 		alias: {
