788d77e7ff45367e12aed6a99439f629144d0d9a
Bricks666 <tconckoff82@gmail.com>
2024-11-25 23:27:32 +0400

    refactor(shared): add utility type to write funtion in short

--- src/shared/types/common.ts
+++ src/shared/types/common.ts
@@ -1,13 +1,17 @@
 import { Effect, Event } from 'effector';
-import { Template, String, Static } from 'runtypes';
+import { Template, String } from 'runtypes';
 
 export const hex = Template`#${String.withConstraint(
 	(code) => code.length === 3 || code.length === 6
 )}`;
-export type HEX = Static<typeof hex>;
+export type HEX = `#${string}`;
 
-export type AnyFunction = (...args: any) => any;
-export type VoidFunction = () => void;
+export type Fn<Params extends Array<unknown>, Result> = (
+	...args: Params
+) => Result;
+
+export type AnyFunction = Fn<any[], any>;
+export type VoidFunction = Fn<[], void>;
 
 export interface ChainedParams {
 	readonly otherwise?: Event<any> | Effect<any, any>;
