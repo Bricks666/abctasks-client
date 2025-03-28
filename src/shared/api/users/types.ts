f1b72bc03290b6376a244d0f5995098a54d972ed
Bricks666 <tconckoff82@gmail.com>
2025-03-28 23:48:51 +0400

    refactor(users): update users requests types

--- src/shared/api/users/types.ts
+++ src/shared/api/users/types.ts
@@ -1,3 +1,14 @@
-export interface SearchUsersQuery {
+import { StandardResponse } from '@/shared/types';
+
+export interface UserDto {
+	readonly id: number;
+	readonly email: string;
 	readonly username: string;
+	readonly photo: string | null;
 }
+export type UsersDto = UserDto[];
+
+export interface GetUsersRequestParams {
+	readonly username?: string | null;
+}
+export type GetUsersResponseData = Promise<StandardResponse<UsersDto>>;
