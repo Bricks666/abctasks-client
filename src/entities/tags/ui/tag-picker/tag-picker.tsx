f5f5f3a43739f9d89902e64a341e420e09a40c49
Bricks666 <tconckoff82@gmail.com>
2025-04-06 01:01:45 +0400

    refactor(shared): extend field props. Make it more common

--- src/entities/tags/ui/tag-picker/tag-picker.tsx
+++ src/entities/tags/ui/tag-picker/tag-picker.tsx
@@ -1,30 +1,57 @@
 import { Autocomplete } from '@mui/material';
 import { FC, memo } from 'react';
 
+import { preparePickerHandler, preparePickerSelectedValue } from '@/shared/lib';
 import { CommonProps, Fn, PickerProps } from '@/shared/types';
 import { Field, FieldProps } from '@/shared/ui';
 
-import { Tag, Tags } from '../../models';
+import { TagId, Tags } from '../../models';
 import { TagLabel } from '../tag-label';
 import { TagListItemTemplate } from '../tag-list-item-template';
 
 export type TagPickerProps = CommonProps &
-	PickerProps<Tag> &
+	PickerProps<TagId> &
 	Omit<FieldProps, 'onChange' | 'value' | 'className' | 'multiline'> & {
 		readonly onInputChange?: Fn<[value: string], void>;
 		readonly tags: Tags;
+		readonly loading?: boolean;
 	};
 
 export const TagPicker: FC<TagPickerProps> = memo((props) => {
-	const { className, onChange, value, limitTags, multiple, tags, ...rest } =
-		props;
+	const {
+		className,
+		onChange,
+		value,
+		limitTags,
+		multiple,
+		tags,
+		loading,
+		...rest
+	} = props;
+
+	const handleChange = preparePickerHandler(
+		{
+			onChange,
+			multiple,
+		},
+		'id'
+	);
+	const selected = preparePickerSelectedValue(
+		{
+			value,
+			multiple,
+		},
+		tags,
+		'id'
+	);
 
 	return (
 		<Autocomplete
 			className={className}
 			options={tags}
-			onChange={onChange}
-			value={value}
+			loading={loading}
+			value={selected}
+			onChange={handleChange}
 			getOptionLabel={(tag) => tag.name}
 			renderOption={(props, tag) => (
 				<TagListItemTemplate {...props} {...tag} key={tag.id} />
